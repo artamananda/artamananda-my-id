f5fe168c97dfcb9a4f567ae3c40bbeca7b404bc0
Artamananda <artamananda@gmail.com>
2025-06-22 23:42:14 +0700

    MAJOR: migrate to svelte + vite

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'artamananda-my-id',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
-      watch: true,
-      exp_backoff_restart_delay: 100,
-    },
-  ],
+      script: 'serve',
+      watch: false
+    }
+  ]
 }
---
+++ src/lib/info.js
@@ -0,0 +1,25 @@
+// This is for convenience as you start out with this template.
+// If you'd rather hardcode these values into your pages,
+// feel free to remove this file.
+
+// URL of your website
+export const website = 'https://sveltekit-blog-template.vercel.app'
+
+export const firstName = 'Artamananda'
+export const lastName = '.'
+export const name = `${firstName}${lastName}`
+
+// Used for the landing page & footer of posts
+export const avatar =
+  'https://pub-6129975179ce43e5b9eacdff0920180a.r2.dev/artamananda-my-id/profile.jpg'
+export const bio = `\
+I’m ${firstName}, a software developer based in South Sumatera, Indonesia. I focused on building modern web and mobile applications. I use TypeScript, React, Svelte, and Next.js for high-performance frontends, and React Native for seamless cross-platform mobile apps. On the backend, I work with NestJS and GoFiber (Golang) to create scalable and efficient APIs. I’m passionate about exploring new technologies, optimizing performance, and building innovative end-to-end solutions.
+`
+
+// Your social usernames, empty strings won't be used.
+// For sake of the live demo, I'm using `username/repo` for github,
+// but you can replace with just your username.
+export const github = 'artamananda'
+export const twitter = ''
+export const linkedin = 'artamananda'
+export const instagram = 'artamananda'
