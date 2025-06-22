6bed4a827855762d90a11b8190e5a314330e1684
Artamananda <artamananda@gmail.com>
2025-06-23 00:31:33 +0700

    adjust config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'artamananda-my-id',
-      script: 'node build/index.js',
+      script: 'node',
+      args: 'build/index.js',
       watch: false,
       exp_backoff_restart_delay: 100,
     }
