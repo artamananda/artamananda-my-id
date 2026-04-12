52db0b973da14ffe016ef6bf6417310c4b30898e
artamananda <artamananda@gmail.com>
2026-04-12 13:05:46 +0700

    feat: comment out external advertising scripts and links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -88,14 +88,14 @@
         ></script>
         <div id="container-43cfc0ee17e03bd04b7165471b9695e9"></div>
         <!-- <script src="https://pl29130736.profitablecpmratenetwork.com/b8/fe/47/b8fe47b89ba02b87f947bfa1b7a19556.js"></script> -->
-        <a
+        <!-- <a
             href="https://www.profitablecpmratenetwork.com/a2qf1g4z?key=cad550e67a4e80eb00db48b9328bc14f"
             target="_blank"
             rel="noopener noreferrer"
             class="text-xs text-teal-500 dark:text-teal-400 hover:underline"
           >
             Visit link
-          </a>
+          </a> -->
         <!-- <script src="https://pl29130738.profitablecpmratenetwork.com/65/77/90/65779097e6eedffaa768cc7398a9e543.js"></script> -->
       </div>
     </section>
