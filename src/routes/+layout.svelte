7141d6dcc2b07d747e625a8e69920c678ca38123
artamananda <artamananda@gmail.com>
2026-04-12 13:12:52 +0700

    feat: comment out external advertising script tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -71,7 +71,7 @@
     </main>
     <section class="w-full max-w-2xl mx-auto overflow-x-auto py-4">
       <div class="min-w-[728px]">
-        <script>
+        <!-- <script>
           atOptions = {
             'key' : 'c1edeec1394dd5fc423c1acca7bcf821',
             'format' : 'iframe',
@@ -80,7 +80,7 @@
             'params' : {}
           };
         </script>
-        <script src="https://www.highperformanceformat.com/c1edeec1394dd5fc423c1acca7bcf821/invoke.js"></script>
+        <script src="https://www.highperformanceformat.com/c1edeec1394dd5fc423c1acca7bcf821/invoke.js"></script> -->
         <script
           async="async"
           data-cfasync="false"
